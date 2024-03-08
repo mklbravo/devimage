ef21696cdc2df4f98f09c74cdfb8fb79a4410451
Mikel Bravo <me@mklbravo.net>
2024-03-08 14:33:35 +0100

    feat(setup-lazygit): add script

---
+++ rootfs/scripts/setup-lazygit.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# See: https://github.com/jesseduffield/lazygit
+echo "Setting-Up Lazygit..."
+
+echo "Installing required packages..."
+/scripts/package-manager.sh install \
+    curl \
+    tar
+
+echo "Getting Lazygit latest version..."
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+
+echo "Downloading Lazygit..."
+curl -Lo /tmp/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+
+echo "Extracting Lazygit..."
+tar xf /tmp/lazygit.tar.gz --directory /tmp lazygit
+
+echo "Moving Lazygit to /usr/local/bin..."
+install /tmp/lazygit /usr/local/bin
+
+echo "Removing Lazygit package..."
+rm -rf /tmp/lazygit*
