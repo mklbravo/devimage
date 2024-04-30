7cadb2c61458d4f88fbde35113129ea06b3d38a5
Mikel Bravo <me@mklbravo.net>
2024-04-30 12:15:33 +0200

    feat(setup-secrets): add github as known_hosts

--- rootfs/build-scripts/setup-secrets.sh
+++ rootfs/build-scripts/setup-secrets.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Prepare the SSH configuration
 mkdir -p /root/.ssh
 ln -s /run/secrets/ssh_private_key /root/.ssh/id_rsa
+touch /root/.ssh/known_hosts
+ssh-keyscan -H github.com >>/root/.ssh/known_hosts
