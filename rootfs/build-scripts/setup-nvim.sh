6414f5686c55833acecd52d588fff0245c950ba6
Mikel Bravo <me@mklbravo.net>
2024-10-17 16:07:44 +0200

    fix: add quotes to variable

--- rootfs/build-scripts/setup-nvim.sh
+++ rootfs/build-scripts/setup-nvim.sh
@@ -41,7 +41,7 @@ echo "Creating Neovim symlink..."
 ln -s "$INSTALL_FOLDER/AppRun" /usr/bin/nvim
 
 echo "Cloning Neovim configuration..."
-git clone --depth 1 --branch ${NVIM_CONFIG_VERSION} https://github.com/mklbravo/nvim-config.git ~/.config/nvim
+git clone --depth 1 --branch "${NVIM_CONFIG_VERSION}" https://github.com/mklbravo/nvim-config.git ~/.config/nvim
 
 echo "Install Lazy dependencies..."
 nvim --headless +Lazy install +qall
